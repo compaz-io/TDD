5c0878e5d5b10e9f9665b07aa88e98764e79b901
Schweigs <mjschweiger@gmail.com>
2021-09-26 17:49:33 +0100

    changes to functional tests and home.html

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -20,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-
+        print(header_text)
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
@@ -34,25 +36,32 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
+        
+        # table = self.browser.find_element_by_id('id_list_table')
+        self.browser.implicitly_wait(3)
+        time.sleep(1)
+        table = self.browser.find_element_by_tag_name('table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
-
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        
         # The page updates again, and now shows both items on her list
-
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. The she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,18 +1,42 @@
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
+import re
 from django.test import TestCase
 from .views import home_page
 
 class HomePageTest(TestCase):
+
+    def remove_csrf_tag(self, text):
+    #Remove csrf tag from TEXT
+        return re.sub(r'<[^>]*csrfmiddlewaretoken[^>]*>', '', text)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         print("the output of found is:", found)
         self.assertAlmostEqual(found.func, home_page)
-
+    
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+
+        self.assertEqual(
+            self.remove_csrf_tag(response.content.decode()), 
+            self.remove_csrf_tag(expected_html)
+            )
+    
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode(), request.POST)
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'}, request=request)
+        self.assertEqual(
+            self.remove_csrf_tag(response.content.decode()),
+            self.remove_csrf_tag(expected_html))
